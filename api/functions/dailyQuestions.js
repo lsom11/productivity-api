680c42a96459900e29705bf21720198aaebc5ee2
Luc <lucwsomers@gmail.com>
2019-05-11 17:56:19 -0300

    CREATE  - serverless, microservices

---
+++ api/functions/dailyQuestions.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports.add = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully! POST',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+};
+
+module.exports.get = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully! GET',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+};
